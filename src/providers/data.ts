5af0b7bceecece36956ee0d415aa257641058673
sepehrsylvanus <mr.forte.world@gmail.com>
2026-02-02 13:04:11 +0300

    finalize create classes form

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,5 +1,5 @@
 import { BACKEND_BASE_URL } from "@/constants";
-import { ListResponse } from "@/types";
+import { CreateResponse, ListResponse } from "@/types";
 import { HttpError } from "@refinedev/core";
 import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest";
 
@@ -63,6 +63,18 @@ const options: CreateDataProviderOptions = {
       const payload: ListResponse = await response.clone().json();
 
       return payload.pagination?.total ?? payload.data?.length ?? 0;
+    },
+  },
+
+  create: {
+    getEndpoint: ({ resource }) => resource,
+
+    buildBodyParams: async ({ variables }) => variables,
+
+    mapResponse: async (response) => {
+      const json: CreateResponse = await response.clone().json();
+
+      return json.data ?? [];
     },
   },
 };
